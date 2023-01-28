2a9d326ca995c3c3cfdf619f7755f75e7ca9590d
Maksym Kotyk <mxm.kotyk@gmail.com>
2023-01-28 10:34:35 +0100

    Done

--- lesson2.js
+++ lesson2.js
@@ -1,17 +1,30 @@
-// Створіть масив styles з елементами 'Джаз' і 'Блюз'
+// 1. Створіть масив styles з елементами 'Джаз' і 'Блюз'
 //Додайте в кінець 'Рок-н-ролл'
 //Заменіть значення 'Блюз' на 'Класика'
 //Видаліть перший елемент масива та виведіть його в консоль
 // Додайте 'Реп' та 'Реггі' на початок списку.
-const styles = ['Джаз', 'Блюз'];
-styles.push('Рок-н-ролл');
+// const styles = ['Джаз', 'Блюз'];
+// styles.push('Рок-н-ролл');
 
-// styles[1] = 'Класика';
+// // styles[1] = 'Класика';
 
- let idex = styles.indexOf('Блюз');
-// styles[idex] = 'Класика';
+//  let idex = styles.indexOf('Блюз');
+// // styles[idex] = 'Класика';
 
-styles.splice(idex, 1, 'Класика');
-console.log(styles.shift());
-styles.unshift('Реп', 'Реггі');
-console.log(styles);
+// styles.splice(idex, 1, 'Класика');
+// console.log(styles.shift());
+// styles.unshift('Реп', 'Реггі');
+// console.log(styles);
+
+// 2. Напишіть функцію logItems (array), яка приймає
+// масив і використовує цикл for, який для кожного елемента
+//буде виводити повідомлення у форматі:
+//<номер елемента> - <значення елемента>
+//Нумерація має починатись з 1
+
+function logItems(array) {
+  for (let i = 0; i < array.length; i += 1) {
+    console.log(`${i + 1} - ${array[i]}`);
+  }
+}
+logItems(["Джаз", "Блюз", "Рок-н-ролл", "Реп", "Реггі"]);
