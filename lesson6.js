499851f328fd5e54e94a67b563d9c5608379c2e7
VolynetsOleksii <volinetsaleksey@gmail.com>
2023-03-25 12:47:18 +0200

    task1

---
+++ lesson6.js
@@ -0,0 +1,48 @@
+// let i = 0;
+// // setTimeout(() => alert(i), 100);
+// setTimeout(() => alert(i), 0);
+
+// for (let j = 0; j < 1000000000; j++) {
+//   i++;
+// }
+
+// =================================================
+
+// const first = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 500, "один");
+// });
+
+// const second = new Promise((resolve, reject) => {
+//   setTimeout(resolve, 100, "два");
+// });
+
+// Promise.race([first, second]).then((res) => console.log(res));
+
+// =================================================
+
+// setTimeout(() => console.log(1), 0);
+
+// console.log(2);
+
+// new Promise((res) => {
+//   console.log(3);
+//   res();
+// }).then(() => console.log(4));
+
+// console.log(5);
+
+// ================================================
+
+// Перероби функцію на проміс таким чином, щоб проміс повертав значення
+// через 2 секунди після виклику функції
+
+// function greet() {
+
+//   return "hello world";
+// }
+
+const promise = new Promise(resolve => {
+    setTimeout(() => resolve("hello world"), 2000)
+})
+
+promise.then(resp => console.log(resp))
